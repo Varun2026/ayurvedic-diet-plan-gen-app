f03560a2e3d5ac0a74e3932d0801362a2f35d942
VarunK20 <varuncse30@gmail.com>
2025-09-20 17:38:53 +0530

    Fixing diet plan generation

--- backend/routes/dietPlan.route.js
+++ backend/routes/dietPlan.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { generateDietPlan } = require('../controllers/dietPlan.controller');
+const { protect } = require('../middleware/auth.middleware'); // Make sure protect is imported
 
-router.post('/generate', generateDietPlan);
+// The 'protect' middleware is now added to this route
+router.post('/generate', protect, generateDietPlan);
 
 module.exports = router;
